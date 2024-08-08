3752856ed1582a47e8ed483ec0f2b0f8b9deea84
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-08-07 21:23:38 -0600

    audio playing + skipping

--- openFile.gd
+++ openFile.gd
@@ -1,5 +1,7 @@
 extends Button
 
+var songs = []
+var index = 0
 var a = null
 var b= null
 var c= null
@@ -12,14 +14,15 @@ var h= null
 func _ready():
 	pass
 
+func _process(delta):
+	print(index)
+
 func load_and_play(path):
 	$AudioStreamPlayer2D.stream = AudioStreamOggVorbis.load_from_file(path)
+	$AudioStreamPlayer2D.play()
 
 func _on_file_open_file_selected(path):
-	load_and_play(path)
-	print(path)
-	dir_contents(path)
-
+	pass
 
 func _on_button_up():
 	$fileOpen.popup()
@@ -31,32 +34,27 @@ func dir_contents(path):
 		var file_name = dir.get_next()
 		while file_name != "":
 			if file_name.ends_with(".ogg"):
-				if a == null:
-					a = path + "/" + file_name
-				elif b == null:
-					b = path + "/" + file_name
-				elif c == null:
-					c = path + "/" + file_name
-				elif d == null:
-					d = path + "/" + file_name
-				elif e == null:
-					e = path + "/" + file_name
-				elif f == null:
-					f = path + "/" + file_name
-				elif g == null:
-					g = path + "/" + file_name
-				elif h == null:
-					h = path + "/" + file_name
+				songs.append(path + "/" + file_name)
 			if dir.current_is_dir():
 				print("Found directory: " + file_name)
 			else:
 				print("Found file: " + file_name)
 			file_name = dir.get_next()
+		load_and_play(songs[0])
+		
 	else:
 		print("An error occurred when trying to access the path.")
-		
-	load_and_play(a)
 
 
 func _on_file_open_dir_selected(dir):
 	dir_contents(dir)
+
+
+func _on_skip_button_up():
+	index += 1
+	load_and_play(songs[index])
+
+
+func _on_back_button_up():
+	index -= 1
+	load_and_play(songs[index])
